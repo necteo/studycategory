ceaa6d97132c5888eae6e09ded74a3361ce2f28b
necteo <cauresn06@gmail.com>
2023-05-24 23:08:25 +0900

    M: desc margin

--- src/components/main/Action.js
+++ src/components/main/Action.js
@@ -10,18 +10,17 @@ const Action = ({ statements, checkedNum, pageNum, setPageNum, score }) => {
       // 해당 페이지 문항에 전부 응답하면
       if (pageNum < statements.length) {
         // 마지막 페이지가 아닐 때
-        window.scrollTo({ top: 0, behavior: "smooth" }); // 맨 위로 스크롤
+        window.scrollTo({ top: 1, behavior: "smooth" }); // 맨 위로 스크롤
         setPageNum(pageNum + 1);
         const radios = document.querySelectorAll("input");
         radios.forEach((radio) => (radio.checked = false)); // 모든 라디오 버튼의 체크 해제
-        return;
       } else if (pageNum === statements.length) {
         // 마지막 페이지일 떄
         navigate("/result", { state: { score: { ...score } } }); // 결과 페이지로
-        return;
       }
+    } else {
+      alert("응답하지 않은 문항이 있습니다.");
     }
-    alert("응답하지 않은 문항이 있습니다.");
   };
 
   return (
