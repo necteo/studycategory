b1b8b3a0e0ae301c682502c15cea9474b0ee2abd
necteo <cauresn06@gmail.com>
2023-08-04 23:21:12 +0900

    상세결과페이지 더보기 기능 추가

--- src/components/result/Descriptions.js
+++ src/components/result/Descriptions.js
@@ -1,12 +1,26 @@
 import { useLocation } from "react-router";
 import results from "../../assets/strings/results.json";
 import "../../css/Descriptions.css";
+import { useState } from "react";
 
 const Descriptions = () => {
+  const [more, setMore] = useState({
+    positive: false,
+    systematic: false,
+    active: false,
+    holistic: false,
+  });
+
   const location = useLocation();
   const studyType = location.state.studyType;
   const score = location.state.score;
   const maxScore = location.state.maxScore;
+
+  const setMoreOnClick = (type) => {
+    const newMore = { ...more };
+    newMore[type] = !more[type];
+    setMore(newMore);
+  };
 
   return (
     <div className="descriptions">
@@ -69,23 +83,37 @@ const Descriptions = () => {
       {Object.keys(results.descriptions).map((type) => (
         <div className="description" key={type}>
           {score[type]["value"] === maxScore[type]
-            ? results.descriptions[type][4].map((sentence) => (
-                <div>
-                  <div>{sentence}</div>
-                  <h2></h2>
-                </div>
-              ))
-            : results.descriptions[type][
+            ? results.descriptions[type]["main"][4]
+            : results.descriptions[type]["main"][
                 parseInt(
                   ((score[type]["value"] / maxScore[type]) * 100).toFixed(0) /
                     20
                 )
-              ].map((sentence) => (
-                <div>
-                  <div>{sentence}</div>
-                  <h2></h2>
-                </div>
-              ))}
+              ]}
+          <h2></h2>
+          {more[type] === true
+            ? score[type]["value"] === maxScore[type]
+              ? results.descriptions[type]["details"][4].map((sentence) => (
+                  <div>
+                    <div>{sentence}</div>
+                    <h2></h2>
+                  </div>
+                ))
+              : results.descriptions[type]["details"][
+                  parseInt(
+                    ((score[type]["value"] / maxScore[type]) * 100).toFixed(0) /
+                      20
+                  )
+                ].map((sentence) => (
+                  <div>
+                    <div>{sentence}</div>
+                    <h2></h2>
+                  </div>
+                ))
+            : null}
+          <button onClick={() => setMoreOnClick(type)}>
+            {more[type] === false ? "더보기" : "접기"}
+          </button>
         </div>
       ))}
     </div>
